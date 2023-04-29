2cd393af6844634ad877377e500280ad31259541
Lorin Speybrouck <Lorin.Speybrouck@UGent.be>
2023-04-29 11:28:38 +0200

    Add lilypond activation

--- src/extension.ts
+++ src/extension.ts
@@ -20,12 +20,9 @@ export function activate(context: vscode.ExtensionContext) {
     });
 
     // 👍 formatter implemented using API
-    vscode.languages.registerDocumentFormattingEditProvider('foo-lang', {
-        provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
-            const firstLine = document.lineAt(0);
-            if (firstLine.text !== '42') {
-                return [vscode.TextEdit.insert(firstLine.range.start, '42\n')];
-            }
+    vscode.languages.registerDocumentFormattingEditProvider('lilypond', {
+        provideDocumentFormattingEdits(document: vscode.TextDocument) {
+            return [vscode.TextEdit.insert(new vscode.Position(0, 0), "hello world")];
         }
     });
 }

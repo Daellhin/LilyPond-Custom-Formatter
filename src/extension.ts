67d07d2a83f7203b06d61552a01fd12d64ccfec7
Lorin Speybrouck <Lorin.Speybrouck@UGent.be>
2023-04-29 19:22:45 +0200

    First itteration, unifies lines before comment

--- src/extension.ts
+++ src/extension.ts
@@ -2,14 +2,38 @@
 
 import * as vscode from 'vscode';
 
+function findFirstBracketPairEndIndex(string: string, openChar = "{", closeChar = "}") {
+    let openBrackets = 0;
+    //string = [...string].filter(e => e === openChar || e === closeChar).join('');
+    for (const [i, char] of string.split("").entries()) {
+        if (char === openChar) {
+            openBrackets++
+        } else if (char === closeChar) {
+            openBrackets--;
+            if (openBrackets === 0) return i;
+            if (openBrackets < 0) return i; // invalid brackets
+        }
+    }
+    return -1; // no  brackets found
+}
+
+function findNthIndex(string: string, char: string, n: number) {
+    let index = -1;
+    for (let i = 0; i < n; i++) {
+        index = string.indexOf(char, index + 1);
+        if (index === -1) break; // end of string reached, nth char not found
+    }
+    return index;
+}
+
 export function activate(context: vscode.ExtensionContext) {
 
     // 👎 formatter implemented as separate command
     vscode.commands.registerCommand('extension.format-foo', () => {
-        const {activeTextEditor} = vscode.window;
+        const { activeTextEditor } = vscode.window;
 
         if (activeTextEditor && activeTextEditor.document.languageId === 'foo-lang') {
-            const {document} = activeTextEditor;
+            const { document } = activeTextEditor;
             const firstLine = document.lineAt(0);
             if (firstLine.text !== '42') {
                 const edit = new vscode.WorkspaceEdit();
@@ -22,9 +46,57 @@ export function activate(context: vscode.ExtensionContext) {
     // 👍 formatter implemented using API
     vscode.languages.registerDocumentFormattingEditProvider('lilypond', {
         provideDocumentFormattingEdits(document: vscode.TextDocument) {
-            return [vscode.TextEdit.insert(new vscode.Position(0, 0), "hello world")];
+            const text = document.getText()
+            // -- Cleanup --
+            const cleanedText = text
+                .replace(/ *= */g, ' = ') // remove spaces around =
+
+            // -- Format -- 
+            const matchedBlocks = Array.from(cleanedText
+                .matchAll(/(?<=((^|}|"|%.*)(\s|\n)*))(?<!({})(\s|\n)*)((\\version)|(\\header)|(\\layout)|((.*=\s*)?\\relative)|(\\score)|(%(?!.*\n*.*(}|%))))/g))
+            const blocks1 = matchedBlocks
+                .map(match => {
+                    let endIndex = 0;
+                    const matchTextAndLeftover = cleanedText.slice(match.index, cleanedText.length)
+                    if (match[0].startsWith("%"))
+                        endIndex = matchTextAndLeftover.indexOf("\n") - 1
+                    else if (match[0].startsWith("\\version"))
+                        endIndex = findNthIndex(matchTextAndLeftover, `"`, 2)
+                    else
+                        endIndex = findFirstBracketPairEndIndex(matchTextAndLeftover)
+
+                    const matchText = matchTextAndLeftover.slice(0, endIndex + 1)
+                    return { text: matchText, index: match.index }
+                })
+            const blocks2 = blocks1
+                .filter((match, _, array) => {
+                    const startIndex = match.index
+                    const endIndex = match.index + match.text.length
+                    array.forEach((otherMatch, otherIndex) => {
+                        if (otherIndex !== match.index) return
+                        const otherStartIndex = otherMatch.index
+                        const otherEndIndex = otherMatch.index + otherMatch.text.length
+                        if (otherStartIndex < startIndex && otherEndIndex < endIndex)
+                            return false
+                    })
+                    return true;
+                })
+            const blocks3 = blocks2
+                .map(match => {
+                    if (match.text.includes("\\relative")) {
+                        const preamble = match.text.match(/.*?\\relative .*?{/g)
+                        const body = match.text.replace(preamble[0], '').trim()
+                        const rows = body.match(/.*?%.*?$/gms)
+                        const newRows = rows.map(row => "\t" + row.replace(/[\n\r]/g, '').trim())
+                            .join('\n')
+                        return preamble + "\n" + newRows + "\n}"
+                    }
+                    return match.text
+                })
+            const formattedBlocks = blocks3.join('\n')
+
+            const wholeDocument = new vscode.Range(document.positionAt(0), document.positionAt(text.length))
+            return [vscode.TextEdit.replace(wholeDocument, formattedBlocks)]
         }
     });
 }
-
-
